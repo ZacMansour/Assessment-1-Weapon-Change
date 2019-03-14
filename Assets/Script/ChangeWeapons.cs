af102cdbe79cb57c231a5d0a1be8d7bc58bbb332
ZacMansour <47873884+ZacMansour@users.noreply.github.com>
2019-03-14 19:57:06 +1100

    Assets File Upload

---
+++ Assets/Script/ChangeWeapons.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeWeapons : MonoBehaviour 
+
+{
+    //Declare WeaponsArray of GameObjects + set in Unity
+    public GameObject[] weapons;
+
+    void Start () 
+    {
+        Cursor.visible = false;//hide cursor
+
+        //set weapon to default
+        WeaponChanger(0);
+    }
+
+    void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            WeaponChanger(0);
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            WeaponChanger(1);
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            WeaponChanger(2);
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+            WeaponChanger(3);
+
+    }
+
+    //function(passedValue)
+    //for (1 to WeaponsArray.Length)
+    //if (loopValue == passedValue)
+    //WeaponsArray [current element].SetActive(true)
+    //else 
+    //WeaponsArray [current element].SetActive(false)
+
+    void WeaponChanger(int weaponNumber)
+    {
+        for (int i = 0; i < weapons.Length; i++)
+        {
+            weapons[i].SetActive(false);
+            if (i != weaponNumber)
+                weapons[i].SetActive(false);
+            else
+                weapons[i].SetActive(true);
+        }
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 0.05f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Input.GetKeyDown(KeyCode.W))
+        {
+            float moveVertical = (Input.GetAxis("Vertical") * speed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            //transform.Translate(new Vector3(1, 0, 0));
+            float moveHorizontal = (Input.GetAxis("Horizontal") * speed);
+        }*/
+
+        float moveHorizontal = (Input.GetAxis("Horizontal"));
+        float moveVertical = (Input.GetAxis("Vertical"));
+
+        transform.Translate(new Vector3(moveHorizontal, 0.0f, moveVertical) * speed);
+    }
+}
